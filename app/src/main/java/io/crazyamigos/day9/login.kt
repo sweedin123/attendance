f5645c4f13204163e756a43bc5b1bf70d51ba027
sweedin <sweedinpp@gmail.com>
2018-07-26 13:13:28 +0530

    initial commit,login page done

---
+++ app/src/main/java/io/crazyamigos/day9/login.kt
@@ -0,0 +1,74 @@
+package io.crazyamigos.day9
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
+import org.json.JSONObject
+
+class login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun doLogin(view: View) {
+        if (username.text.toString().isEmpty() || password.text.toString().isEmpty())
+            toast("invalid username or password!!")
+        else {
+            login()
+        }
+        fun login() {
+            progress.visibility = View.VISIBLE
+            doAsync {
+
+                val body = FormBody.Builder()
+                        .add("username", username.text.toString())
+                        .add("password", password.text.toString())
+                        .build()
+                val request = Request.Builder()
+                        .url("https://test3.htycoons.in/api/login")
+                        .build()
+
+                val client = OkHttpClient()
+
+                val response = client.newCall(request).execute()
+
+                uiThread {
+                    progress.visibility = View.INVISIBLE
+                    when (response.code()) {
+                        200 -> {
+                            if (response.body() != null) {
+                                val jsonResponse = JSONObject(response.body()!!.string())
+                                val accessToken = jsonResponse.getString("access_token")
+                                Log.d("ACCESS", accessToken)
+                                val pref = getSharedPreferences("event",0)
+                                val editor = pref.edit()
+                                editor.putString("access_token",accessToken)
+                                editor.apply()
+                                startActivity(intentFor<event>())
+                                finish()
+                            }
+                        }
+                        400 -> {
+
+                        }
+                        404 -> {
+
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
